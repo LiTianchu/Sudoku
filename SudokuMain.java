4c33c5e41e795d5d5c3bcb7b0191793d29617bf2
qingerr <charisqinger@gmail.com>
2022-04-06 17:44:12 +0800

    new game btn, no longer needs to press enter

--- SudokuMain.java
+++ SudokuMain.java
@@ -1,10 +1,10 @@
-import java.awt.*; 
-import java.awt.event.*; 
-import javax.swing.*; 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class SudokuMain extends JFrame{
-  
-     // private variables
+public class SudokuMain extends JFrame {
+
+   // private variables
    GameBoard board = new GameBoard();
    JButton btnNewGame = new JButton("New Game");
 
@@ -12,16 +12,24 @@ public class SudokuMain extends JFrame{
    public SudokuMain() {
       Container cp = getContentPane();
       cp.setLayout(new BorderLayout());
-
       cp.add(board, BorderLayout.CENTER);
 
       // Add a button to the south to re-start the game
-      // ......
+      btnNewGame.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 15));
+      JPanel flowPanel = new JPanel(new FlowLayout());
+      flowPanel.add(btnNewGame);
+      cp.add(BorderLayout.EAST, flowPanel);
+
+      // Restart Game
+      btnNewGame.addActionListener(e -> {
+         dispose(); // Close the window
+         SudokuMain sudoku = new SudokuMain();
+      });
 
       board.init();
 
-      pack();     // Pack the UI components, instead of setSize()
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Handle window closing
+      pack(); // Pack the UI components, instead of setSize()
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Handle window closing
       setTitle("Sudoku");
       setVisible(true);
    }
@@ -29,8 +37,7 @@ public class SudokuMain extends JFrame{
    /** The entry main() entry method */
    public static void main(String[] args) {
       // [TODO 1] Check Swing program template on how to run the constructor
-     // ......
-     SudokuMain sudoku = new SudokuMain();
+      SudokuMain sudoku = new SudokuMain();
    }
-    
+
 }
